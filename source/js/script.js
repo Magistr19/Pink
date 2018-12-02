16cf32d8fcf8a5272dc652bcfd84cbd7d5d65ea2
Alex <sashapayareli@gmail.com>
2018-12-02 16:33:45 +0200

    Finished mobile header

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,27 @@
-﻿
+﻿/* Js active */
+
+var bodyElement = document.querySelector ('.no-js');
+bodyElement.classList.remove('no-js');
+
+var pageHeader = document.querySelector ('.page-header__group-container');
+pageHeader.classList.remove ('page-header__group-container--open');
+
+var pageHeaderNav = document.querySelector ('.page-header__nav');
+pageHeaderNav.classList.remove ('page-header__nav--open');
+
+/* Mobile nav */
+
+var toogleBtn = document.querySelector ('.toggle-btn');
+
+toogleBtn.addEventListener("click", function(evt) {
+  if (toogleBtn.classList.contains('toggle-btn--open')) {
+    pageHeader.classList.remove ('page-header__group-container--open');
+    toogleBtn.classList.remove ('toggle-btn--open');
+    pageHeaderNav.classList.remove ('page-header__nav--open');
+  }
+  else {
+    pageHeader.classList.add ('page-header__group-container--open');
+    toogleBtn.classList.add ('toggle-btn--open');
+    pageHeaderNav.classList.add ('page-header__nav--open');
+  }
+});
